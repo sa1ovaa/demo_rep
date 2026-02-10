7ad7b5a64d64449fa243f93d66475edf43d14169
sa1ovaa <a_sabyrova@kbtu.kz>
2026-02-10 18:02:53 +0600

    commit

---
+++ practice 3/classses/classes.py
@@ -0,0 +1,17 @@
+#1 Create an object named p1, and print the value of x:
+p1 = MyClass()
+print(p1.x)
+#Delete the p1 object:
+del p1
+
+
+
+#2 Create three objects from the MyClass class:
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
--- practice 3/lambda/lambda_basics.py
+++ practice 3/lambda/lambda_basics.py
@@ -1,3 +1,33 @@
-#1 Multiply argument a with argument b and return the result:
-x = lambda a, b : a * b
-print(x(5, 6))
+#1Summarize argument a, b, and c and return the result:
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+#
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+#3 double all numbers in a list:
+
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
+
+#4 Filter out even numbers from a list:
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
+
+#5 Sort a list of tuples by the second element:
+
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
